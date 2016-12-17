044db434722c4b8b30b05b4a5edb1a9c830d6d74
Keith Smiley <keithbsmiley@gmail.com>
2016-12-17 13:51:51 -0800

    Layer 1, toggle to layer 2 button

--- keyboard/atreus/keymap_keith.c
+++ keyboard/atreus/keymap_keith.c
@@ -25,13 +25,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Mixed bag row: grave/tilde, 5 blank, equal, left bracket [, right ], backslash
   // Arrow row: 6 blank, left, down, up, right
   /// Bottom Row
-  // Ctrl, option/alt, GUI, shift, space
+  // Switch to layer 2, option/alt, GUI, shift, space
   // ESC, FN (hold with numbers)
   // Backspace, blank (holding to get here), minus, quote, enter
   KEYMAP(KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, \
          KC_GRAVE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_EQUAL, KC_LBRC, KC_RBRC, KC_BSLS, \
          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, \
-         KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, KC_SPC, \
+         KC_FN1, KC_LALT, KC_LGUI, KC_LSFT, KC_SPC, \
          KC_ESC, KC_FN, \
          KC_BSPC, KC_NO, KC_MINS, KC_QUOT, KC_ENT), \
   /// Layer 2
